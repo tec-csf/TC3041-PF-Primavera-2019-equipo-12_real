8adc8f46921bb70925202821fefe2dd27ba39d75
Daniel Pelagio <pelagio3@gmail.com>
2019-05-03 17:59:31 -0500

    Added Login frontend

--- frontend/AMAYA_HTML/main.py
+++ frontend/AMAYA_HTML/main.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, session
 import cgi
 import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/", methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['pass'] != 'admin':
+            error = 'invalid credentials. Please try again.'
+        else:
+            return render_template('home.html')
+    return render_template('index.html', error=error)
+    
 
-@app.route("/hello", methods=['POST'])
-def hello():
-    first_name= request.form['firts_name']
-    template= jinja_env.get_template('hello_greeting.html')
-    return template.render(name=first_name)
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('welcome'))
