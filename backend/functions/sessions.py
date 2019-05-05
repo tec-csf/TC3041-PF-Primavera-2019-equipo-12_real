b315492f5917f90ae5e81332a76d6dbedb0ad397
carrasc <luiscbilbao@gmail.com>
2019-05-04 20:17:55 -0500

    Se sgrego el api para manejar las sesiones

---
+++ backend/functions/sessions.py
@@ -0,0 +1,34 @@
+import redis
+import hashlib, uuid
+import config
+
+class Sessions(object):
+
+    def __init__(self):
+        if hasattr(config, 'REDIS_PASSWORD'):
+            self.instance = redis.StrictRedis(
+                host=config.REDIS_HOST,
+                port=config.REDIS_PORT,
+                password=config.REDIS_PASSWORD)
+        else:
+            self.instance = redis.StrictRedis(
+                host= config.REDIS_HOST,
+                port=config.REDIS_PORT)
+
+
+    def h_password(self,password):
+
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        return hashed_password
+    
+    def set_user(self,user,password):
+
+        self.instance.hset(user,"password",Sessions.hashed_pass(self,password))
+
+
+    def get_user_password(self,user):
+
+        password=self.instance.hget(user,"password")
+
+        return password
